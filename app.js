d87e3e6da5f0d7b975220d882acdc31a08b7cef9
Damian Pagowski <d.pagowski@gmail.com>
2020-02-09 22:46:19 +0700

    first working version: displaying data in greed, reading form data, click handlers implemented

--- app.js
+++ app.js
@@ -1,87 +1,103 @@
+// Create Dino Constructor
 
-    // Create Dino Constructor
+// Create Dino Objects
+const grid = document.getElementById("grid");
+const header = document.getElementById("header");
+const form = document.getElementById("dino-compare");
 
+let store = {
+  showGrid: false,
+  dinos: []
+};
 
-    // Create Dino Objects
+// Create Human Object
 
+// Use IIFE to get human data from form
 
-    // Create Human Object
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Use IIFE to get human data from form
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Generate Tiles for each Dino in Array
-  
+// Generate Tiles for each Dino in Array
 const Card = data => {
-    return `<div class="card">
-              <img src="${data.img_src}" class="card-img-top img-fluid">
-              <div class="card-body">
-              <ul class="list-group list-group-flush">
-  <li class="list-group-item">Cras justo odio</li>
-  <li class="list-group-item">Dapibus ac facilisis in</li>
-  <li class="list-group-item">Morbi leo risus</li>
-  <li class="list-group-item">Porta ac consectetur ac</li>
-  <li class="list-group-item">Vestibulum at eros</li>
-</ul>
-              </div>
-          </div>`;
-  };
-  
-  // create content
-  const App = state => {
-    return `
-          ${Navbar()}
-          <header></header>
-          <main>
-             
-              <section>
-                  <h1 class="display-4 text-center my-4">Astronomy Picture of the Day</h1>
-                  <p class="mb-4">
-                      One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                      the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                      This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                      applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                      explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                      but generally help with discoverability of relevant imagery.
-                  </p>
-                  ${ImageOfTheDay(state)}
-              </section>
-              <section id="images">
-              ${Jumbotron(roverSelect(state), RoverDetails(state))}
-              ${Photos(state)}
-              </section>
-  
-          </main>
-          <footer></footer>
-      `;
-  };
-        // Add tiles to DOM
-        const updateStore = (store, newState) => {
-            store = Object.assign(store, newState);
-            render(root, store);
-          };
-          
-          const render = async (root, state) => {
-            root.innerHTML = App(state);
-          };
-    // Remove form from screen
+  return `<div class="grid-item">
+    <h3>${data.species}</h3>
+    <img src="${data.img_src}">
+      <p class="card-text">${data.fact}</p>
+  </div>`;
+};
 
+const Grid = ({ dinos ,  showGrid}) => {
+  return showGrid ? `${dinos.map(dino => Card(dino)).join("")}` : "";
+};
+
+const Header = ({ showGrid }) => {
+  return showGrid
+    ? `<h3 onclick="hideGrid()" class="backArrow"><i class="arrow left"></i> Back</h3>`
+    : `<h2>Natural History Museum</h2> 
+<h1>Dinosaurs</h1>
+<h3>How do you compare?</h3>`;
+};
+
+// Add tiles to DOM
+
+// Remove form from screen
 
 // On button click, prepare and display infographic
+function formSubmitHandler() {
+  const form = document.getElementById("dino-compare");
+  const human = {
+    ishuman: true,
+    species: form.elements["name"].value,
+    weight: form.elements["weight"].value,
+    height:
+      parseInt(form.elements["inches"].value) +
+      parseInt(form.elements["feet"].value) * 12,
+    diet: form.elements["diet"].value,
+    img_src: "images/human.png"
+  };
 
-// listening for load event because page should load before any JS is called
-window.addEventListener("load", () => {
-    getPhotoDataFromRover(store);
-  });
+  const newState = { ...store };
+
+  let dinosCp = [...store.dinos];
+  if (dinosCp[4].ishuman) {
+    dinosCp[4] = human;
+  } else {
+    dinosCp.splice(4, 0, human);
+  }
+  newState.dinos = dinosCp;
+  newState.showGrid = true;
+  updateStore(store, newState);
+}
+
+function hideGrid() {
+  console.log("hide grid");
+
+  const newState = { ...store };
+  newState.showGrid = false;
+  updateStore(store, newState);
+
+}
+
+const updateStore = (store, newState) => {
+  store = Object.assign(store, newState);
+  console.log("updated store: " + JSON.stringify(store));
+  render(store);
+};
+
+const render = async state => {
+  grid.innerHTML = Grid(state);
+  form.style.display = state.showGrid ? "none" : "block";
+  header.innerHTML = Header(state);
+};
+
+// listening for load event
+window.addEventListener("load", async function run() {
+  dinos = await this.fetch("/dino.json").then(res => res.json());
+  updateStore(store, { dinos: dinos.Dinos });
+});
